21782825cf4198c92e209944f51687c93ad0f5f7
Jolkert <poptartking86@gmail.com>
2021-05-19 15:54:51 -0400

    Made the logger synchronous. Fixed all of the problems. Who woulda thought?

--- DexBot/Services/Logger.cs
+++ DexBot/Services/Logger.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace DexBot
 {
 	public static class Logger
 	{// This thing is probably not very good, but I'm gonna use it anyway because I am stubborn and don't want to use another package -Jolkert 2021-05-06
-	 // TODO: Make this thing work better or replace it with a real logging library -Jolkert 2021-05-19
 		private static string _logFile;
 		private static FileStream _stream;
-		private static readonly Queue<string> _writeQueue;
+
+		private static readonly Queue<string> _writeQueue; // We use the queue to prevent collisions
+		private static readonly ThreadStart _writeThreadStart = new ThreadStart(() =>
+		{
+			while (_writeQueue.Count > 0)
+				LogToFileFromQueue();
+		});
+		private static Thread _writeThread;
 
 		static Logger()
 		{
@@ -24,16 +29,23 @@ namespace DexBot
 		public static void LogToFile(string log)
 		{
 			_writeQueue.Enqueue(log);
-			LogToFileFromQueueAsync();
+			if (_writeThread == null || _writeThread.ThreadState == ThreadState.Stopped)
+			{
+				_writeThread = new Thread(_writeThreadStart);
+				_writeThread.Start();
+			}
 		}
-		private static async Task LogToFileFromQueueAsync()
+		private static void LogToFileFromQueue()
 		{
-			await _stream.WriteAsync(Encoding.UTF8.GetBytes($"{_writeQueue.Dequeue()}\n"));
-			if (_writeQueue.Count == 0)
-				RestartStream();
+			_stream.Write(Encoding.UTF8.GetBytes($"{_writeQueue.Dequeue()}\n"));
+			RestartStream();
 		}
 
-		public static void Close() => _stream.Close();
+		public static void Close()
+		{
+			_writeThread.Join();
+			_stream.Close();
+		}
 
 		private static void StartStream()
 		{
@@ -48,7 +60,7 @@ namespace DexBot
 		}
 		private static void RestartStream()
 		{
-			Close();
+			_stream.Close();
 			_stream = new FileStream(_logFile, FileMode.Append);
 		}
 	}
